9b27087e122d6c0417560d7f7cad1c3fe094075b
Ivan Diego Guimarães Lopes <ivandglopes@hotmail.com>
2026-06-05 03:16:42 -0300

    Criado instruções (colocado em ignore) e iniciada alteração de nomes de variaveis

--- src/splitter.py
+++ src/splitter.py
@@ -1,17 +1,20 @@
 #Vamos começar pelos inputs
 
 #Inputs necessários
-actual_total = ""
-num_split = ""
+num_split = int(input("How many people are? "))
 
 #Sub-divisões da conta
-appetizers = 
-main_courses = 
-drinks = 
-desserts = 
+appetizers = int(input("Insert the total value of the appetizers: "))
+main_courses = int(input("Insert the total value of the main courses: "))
+drinks = int(input("Insert the total value of the drinks: "))
+desserts = int(input("Insert the total value of the desserts: "))
+tip = int(input("Insert the total value of the tip: "))
 
 #Adição de gorjeta
-total_tip = actual_total * 0.1 #ou 10/100
+
+total_tip = (appetizers + main_courses + drinks + desserts) * tip_percentage
 
 #Divisão da conta
 total_person = total_tip / num_split
+
+print(total_person)

9cab84456661c63b028324ea385206d0af4d07f3
Ivan Diego Guimarães Lopes <ivandglopes@hotmail.com>
2026-06-05 03:21:45 -0300

    Finalizado inputs e conversões.

--- src/splitter.py
+++ src/splitter.py
@@ -9,12 +9,14 @@ main_courses = int(input("Insert the total value of the main courses: "))
 drinks = int(input("Insert the total value of the drinks: "))
 desserts = int(input("Insert the total value of the desserts: "))
 tip = int(input("Insert the total value of the tip: "))
+total_bill = appetizers + main_courses + drinks + desserts
+print(f"Total of the bill: ${total_bill}")
 
 #Adição de gorjeta
-
-total_tip = (appetizers + main_courses + drinks + desserts) * tip_percentage
+tip_percentage = tip / 100
+total_tip = total_bill * tip_percentage
+print(f"Total tip: ${total_tip}")
 
 #Divisão da conta
 total_person = total_tip / num_split
-
-print(total_person)
+print(f"Total per person: ${total_person}")
